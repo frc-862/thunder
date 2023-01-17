d49a8ff2a7865aa3c249656d222419b22446cc44
Jeffthedoor <jusnoord@gmail.com>
2023-01-16 21:50:48 -0500

    [#17] added neo config class, still untested

---
+++ config/NeoConfig.java
@@ -0,0 +1,16 @@
+package frc.thunder.config;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class NeoConfig {
+  public static CANSparkMax createMotor(int ID, boolean invert, int currentLimit, double voltageCompensation, MotorType motorType, IdleMode idleMode) {
+    CANSparkMax motor = new CANSparkMax(ID, motorType);
+    motor.setInverted(invert);
+    motor.setSmartCurrentLimit(currentLimit);
+    motor.enableVoltageCompensation(voltageCompensation);
+    motor.setIdleMode(idleMode);
+    return motor;
+  }
+}
