bd25293bbda3140bc4f829d3da920d6527a95107
Jeffthedoor <jusnoord@gmail.com>
2023-01-18 20:52:21 -0500

    [#17] added the restorefactorydefaults method

--- config/NeoConfig.java
+++ config/NeoConfig.java
@@ -7,6 +7,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class NeoConfig {
   public static CANSparkMax createMotor(int ID, boolean invert, int currentLimit, double voltageCompensation, MotorType motorType, IdleMode idleMode) {
     CANSparkMax motor = new CANSparkMax(ID, motorType);
+    motor.restoreFactoryDefaults();
     motor.setInverted(invert);
     motor.setSmartCurrentLimit(currentLimit);
     motor.enableVoltageCompensation(voltageCompensation);
