09f646a72ce1763acc9bc2a0691d9432b0d554f1
Jeffthedoor <jusnoord@gmail.com>
2023-02-14 20:34:55 -0500

    [#63] made creategraph function, using arrays. you still have to set the array entry as a graph in shuffleboard

--- shuffleboard/LightningShuffleboard.java
+++ shuffleboard/LightningShuffleboard.java
@@ -1,5 +1,7 @@
 package frc.thunder.shuffleboard;
 
+import java.util.Arrays;
+import org.apache.commons.lang3.ArrayUtils;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -125,6 +127,37 @@ public class LightningShuffleboard {
         } else {
             tab.add(key, defaultValue);
             return defaultValue;
+        }
+    }
+
+    /**
+     * Creates a graph on the shuffleboard, still requires you to set the output as a graph on the shuffleboard
+     * @param tabName
+     * @param key
+     * @param targetValue
+     * @param currentValue
+     */
+    public static void createGraph(String tabName, String key, double targetValue, double... currentValue) {
+        //create the array for the graph
+        double[] value = {targetValue};
+        value = ArrayUtils.addAll(value, currentValue);
+
+        boolean hasComponent = false;
+        ShuffleboardTab tab = Shuffleboard.getTab(tabName);
+
+        //iterate through the components in the tab, check if the component exists
+        for(int i = 0; i < tab.getComponents().size(); i++) {     
+            if(tab.getComponents().get(i).getTitle() == key) {
+                hasComponent = true;
+                break;                
+            }
+        }
+
+        //if the component exists, write to it using the NetworkTable, and if not, create the component
+        if(hasComponent) {
+            NetworkTableInstance.getDefault().getTable("Shuffleboard").getSubTable(tabName).getEntry(key).setDoubleArray(value);
+        } else {
+            tab.add(key, value);
         }
     }
 }
