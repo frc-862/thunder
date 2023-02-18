82962c74019cd2f92150a5b31f356b490c42325e
Burh826 <ka2788698@gmail.com>
2023-02-17 19:52:39 -0500

    Starts WPILIB Data Logging (finished)
    To find the logs, open "Data Log Tool" on your driverstation (downloaded with other wpilib applications such as smartdashboard). Go to download tab in the bottom left and set team number/address to 10.8.62.2 (address of RIO), set username to lvuser, and keep password blank. If the connection is made after logs are created you can just hit the download button and select a file path. Load the file onto AdvantageScope and just select which graphs you want to view. not tested on an actual robot yet but have used a spare roborio and it worked (gave the file) although it gave no values.

--- LightningRobot.java
+++ LightningRobot.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.DataLogManager;
 
 import frc.thunder.auto.Autonomous;
 import frc.thunder.auto.AutonomousCommandFactory;
@@ -83,6 +84,8 @@ public class LightningRobot extends TimedRobot {
     public void robotInit() {
         System.out.println("LightningRobot.robotInit");
 
+        //Starts WPILIB data logging
+        DataLogManager.start();
         // No Live Window for now
         LiveWindow.disableAllTelemetry();
 
