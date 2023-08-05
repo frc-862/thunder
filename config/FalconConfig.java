d825a0fb4176e06dbc11f5f5390cf6407a46d4aa
Fritz Geib <30505127+Fr1tzBot@users.noreply.github.com>
2023-08-05 00:33:50 -0400

    Falcon Config Phoenix 6 Fix

--- config/FalconConfig.java
+++ config/FalconConfig.java
@@ -1,33 +1,33 @@
 package frc.thunder.config;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.ctre.phoenix6.configs.Slot0Configs;
 
 public class FalconConfig {
 
     /**
      * Create TalonFX motor without CAN loop name
      * @param ID CAN ID
-     * @param invert Boolean
-     * @param supplyCurrentLimit Input current limit from the pdh
-     * @param statorCurrentLimit Output current limit for the motor
+     * @param invert Boolean (true = clockwise positive, false = counterclockwise positive)
+     * @param supplyCurrentLimit Input current limit from the pdh (zero to disable)
+     * @param statorCurrentLimit Output current limit for the motor (zero to disable)
      * @param NeutralMode Brake or Coast
      * @return TalonFX motor
      */
-    public static com.ctre.phoenix6.hardware.TalonFX createMotor(int ID, boolean invert, int supplyCurrentLimit, int statorCurrentLimit, NeutralModeValue NeutralMode) {
+    public static TalonFX createMotor(int ID, boolean invert, int supplyCurrentLimit, int statorCurrentLimit, NeutralModeValue NeutralMode) {
         TalonFX motor = new TalonFX(1);
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.MotorOutput.Inverted = invert ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+        config.CurrentLimits.SupplyCurrentLimitEnable = supplyCurrentLimit > 0;
+        config.CurrentLimits.SupplyCurrentLimit = supplyCurrentLimit;
+        config.CurrentLimits.StatorCurrentLimitEnable = statorCurrentLimit > 0;
+        config.CurrentLimits.StatorCurrentLimit = statorCurrentLimit;
+        config.MotorOutput.NeutralMode = NeutralMode;
+        //TODO: supply current limit thresholds
 
-        motor.setInverted(invert);
-        // motor.NeutralModeValue(NeutralMode);
-        var slot0Configs = new Slot0Configs();
-        Slot0Configs.kV = 0.12;
-        Slot0Configs.kP = 0.12;
-        Slot0Configs.kI = 0.12;
-
-        motor.getConfigurator().apply(new TalonFXConfiguration())
-        motor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, supplyCurrentLimit, supplyCurrentLimit, .25), 250);
-        motor.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, statorCurrentLimit, statorCurrentLimit, .25), 250);
+        motor.getConfigurator().apply(config);
         
         return motor;
     }
@@ -42,15 +42,21 @@ public class FalconConfig {
      * @param NeutralMode Brake or Coast
      * @return TalonFX motor
      */
-    public static TalonFX createMotor(int ID, String canbus, boolean invert, int supplyCurrentLimit, int statorCurrentLimit, NeutralMode NeutralMode) {
+    public static TalonFX createMotor(int ID, String canbus, boolean invert, int supplyCurrentLimit, int statorCurrentLimit, NeutralModeValue NeutralMode) {
         TalonFX motor = new TalonFX(1, canbus);
 
-        motor.setInverted(invert);
-        motor.setNeutralMode(NeutralMode);
-        motor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, supplyCurrentLimit, supplyCurrentLimit, .25), 250);
-        motor.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, statorCurrentLimit, statorCurrentLimit, .25), 250);
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.MotorOutput.Inverted = invert ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+        config.CurrentLimits.SupplyCurrentLimitEnable = supplyCurrentLimit > 0;
+        config.CurrentLimits.SupplyCurrentLimit = supplyCurrentLimit;
+        config.CurrentLimits.StatorCurrentLimitEnable = statorCurrentLimit > 0;
+        config.CurrentLimits.StatorCurrentLimit = statorCurrentLimit;
+        config.MotorOutput.NeutralMode = NeutralMode;
+        //TODO: supply current limit thresholds
+
+        motor.getConfigurator().apply(config);
         
         return motor;
-    }    
+    }
 
 }
