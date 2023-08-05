70c05c7e67dcbf054ce7b9e026538b1b0094111d
MattD8957 <Dowlmj8957@gmail.com>
2023-08-05 00:10:14 -0400

    AHHHHHH

--- config/FalconConfig.java
+++ config/FalconConfig.java
@@ -1,9 +1,8 @@
 package frc.thunder.config;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.configs.Slot0Configs;
 
 public class FalconConfig {
 
@@ -16,11 +15,17 @@ public class FalconConfig {
      * @param NeutralMode Brake or Coast
      * @return TalonFX motor
      */
-    public static TalonFX createMotor(int ID, boolean invert, int supplyCurrentLimit, int statorCurrentLimit, NeutralMode NeutralMode) {
+    public static com.ctre.phoenix6.hardware.TalonFX createMotor(int ID, boolean invert, int supplyCurrentLimit, int statorCurrentLimit, NeutralModeValue NeutralMode) {
         TalonFX motor = new TalonFX(1);
 
         motor.setInverted(invert);
-        motor.setNeutralMode(NeutralMode);
+        // motor.NeutralModeValue(NeutralMode);
+        var slot0Configs = new Slot0Configs();
+        Slot0Configs.kV = 0.12;
+        Slot0Configs.kP = 0.12;
+        Slot0Configs.kI = 0.12;
+
+        motor.getConfigurator().apply(new TalonFXConfiguration())
         motor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, supplyCurrentLimit, supplyCurrentLimit, .25), 250);
         motor.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, statorCurrentLimit, statorCurrentLimit, .25), 250);
         
@@ -46,6 +51,6 @@ public class FalconConfig {
         motor.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, statorCurrentLimit, statorCurrentLimit, .25), 250);
         
         return motor;
-    }
+    }    
 
 }
