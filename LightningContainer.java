f356cad49d0b46af3e8b3b82f20409cb87f06e19
MattD8957 <Dowlmj8957@gmail.com>
2024-01-14 21:57:46 -0500

    [#114] Added necessary return type

--- LightningContainer.java
+++ LightningContainer.java
@@ -1,5 +1,7 @@
 package frc.thunder;
 
+import edu.wpi.first.wpilibj2.command.Command;
+
 /**
  * This class is where the bulk of the robot should be declared. Since
  * Command-based is a "declarative" paradigm, very little robot logic should
@@ -59,7 +61,7 @@ public abstract class LightningContainer {
     /**
      * Configures list of possible commands that can be run during autonomous
      */
-    protected abstract void configureAutonomousCommands();
+    protected abstract Command configureAutonomousCommands();
 
     /**
      * Configures all robot-specific fault codes
