6dadaba0821cb3413e346835766f68315fb38363
Jeffthedoor <jusnoord@gmail.com>
2024-02-05 21:29:47 -0500

    lightning shuffleboarded it up

--- tuning/FalconTuner.java
+++ tuning/FalconTuner.java
@@ -3,88 +3,77 @@ package frc.thunder.tuning;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.thunder.shuffleboard.LightningShuffleboard;
 
-public class FalconTuner extends SubsystemBase {
-    private Slot0Configs PIDConfigs;
+public class FalconTuner {
+    private Slot0Configs PIDConfigs = new Slot0Configs();
     private TalonFX motor;
 
-    private GenericEntry kPTuner;
-    private GenericEntry kITuner;
-    private GenericEntry kDTuner;
-    private GenericEntry kSTuner;
-    private GenericEntry kVTuner;
-
-
+    private String tabName;
     private double kP;
     private double kI;
     private double kD;
     private double kS;
     private double kV;
+    private double kA;
 
-    private static int tunerNumber = 0;
+    /**
+     * creates a new FalconTuner, which publishes PIDF gains to shuffleboard and applies them when changed
+     * 
+     * @implNote {@link #update()} must be called periodically to apply/detect changes
+     * @specNote Slot0Configs are refreshed not applied; might retain old values (TODO: check if this is necessary)
+     * 
+     * @param name name of the shuffleboard tab
+     * @param PIDConfigs Slot0Config gains
+     * @param motor motor to tune
+     */
+    public FalconTuner(String name, TalonFX motor, String tabName) {
+        this.motor = motor;
+        this.tabName = tabName;
 
-    public FalconTuner(String name, Slot0Configs PIDConfigs, TalonFX motor) {
-        this.PIDConfigs = PIDConfigs;
-        tunerNumber++;
-
-        ShuffleboardTab tab = Shuffleboard.getTab("PID Tuning");
-
-        if (tunerNumber <= 5) {
-            kPTuner = tab.add(name + "kP", PIDConfigs.kP)
-                    .withPosition(0, tunerNumber).getEntry();
-            kITuner = tab.add(name + "kI", PIDConfigs.kI)
-                    .withPosition(1, tunerNumber).getEntry();
-            kDTuner = tab.add(name + "kD", PIDConfigs.kD)
-                    .withPosition(2, tunerNumber).getEntry();
-            kSTuner = tab.add(name + "kS", PIDConfigs.kS)
-                    .withPosition(3, tunerNumber).getEntry();
-            kVTuner = tab.add(name + "kV", PIDConfigs.kV)
-                    .withPosition(4, tunerNumber).getEntry();
-
-        } else {
-            kPTuner = tab.add(name + "kP", PIDConfigs.kP)
-                    .withPosition(3, tunerNumber).getEntry();
-            kITuner = tab.add(name + "kI", PIDConfigs.kI)
-                    .withPosition(4, tunerNumber).getEntry();
-            kDTuner = tab.add(name + "kD", PIDConfigs.kD)
-                    .withPosition(5, tunerNumber).getEntry();
-            kSTuner = tab.add(name + "kS", PIDConfigs.kS)
-                    .withPosition(6, tunerNumber).getEntry();
-            kVTuner = tab.add(name + "kV", PIDConfigs.kV)
-                    .withPosition(4, tunerNumber).getEntry();
-        }
+        LightningShuffleboard.setDouble(tabName, "kP", 0d);
+        LightningShuffleboard.setDouble(tabName, "kI", 0d);
+        LightningShuffleboard.setDouble(tabName, "kD", 0d);
+        LightningShuffleboard.setDouble(tabName, "kS", 0d);
+        LightningShuffleboard.setDouble(tabName, "kV", 0d);
+        LightningShuffleboard.setDouble(tabName, "kA", 0d);
     }
 
-    private void updateGains() {
-        kP = kPTuner.getDouble(0);
-        kI = kITuner.getDouble(0);
-        kD = kDTuner.getDouble(0);
-        kS = kSTuner.getDouble(0);
-        kV = kVTuner.getDouble(0);
-    }
-
+    /**
+     * @return true if the gains have changed
+     */
     private boolean checkGains() {
-        return kP != kPTuner.getDouble(0) || kI != kITuner.getDouble(0)
-                || kD != kDTuner.getDouble(0) || kS != kSTuner.getDouble(0) || kV != kVTuner.getDouble(0);
+        return  PIDConfigs.kP != kP ||
+                PIDConfigs.kI != kI ||
+                PIDConfigs.kD != kD ||
+                PIDConfigs.kS != kS ||
+                PIDConfigs.kV != kV ||
+                PIDConfigs.kA != kA;
     }
 
-    @Override
-    public void periodic() {
-        if (PIDConfigs != null && checkGains()) {
-            PIDConfigs.kP = kPTuner.getDouble(PIDConfigs.kP);
-            PIDConfigs.kI = kITuner.getDouble(PIDConfigs.kI);
-            PIDConfigs.kD = kDTuner.getDouble(PIDConfigs.kD);
-            PIDConfigs.kS = kSTuner.getDouble(PIDConfigs.kS);
-            PIDConfigs.kV = kVTuner.getDouble(PIDConfigs.kV);
+    /**
+     * updates the gains from shuffleboard and applies them if they have changed
+     * 
+     * @implNote must be called periodically to apply/detect changes
+     */
+    public void update() {
+        kP = LightningShuffleboard.getDouble(tabName, "kP", 0d);
+        kI = LightningShuffleboard.getDouble(tabName, "kI", 0d);
+        kD = LightningShuffleboard.getDouble(tabName, "kD", 0d);
+        kS = LightningShuffleboard.getDouble(tabName, "kS", 0d);
+        kV = LightningShuffleboard.getDouble(tabName, "kV", 0d);
+        kA = LightningShuffleboard.getDouble(tabName, "kA", 0d);
+            
+        if(checkGains()) {
+            PIDConfigs.kP = kP;
+            PIDConfigs.kI = kI;
+            PIDConfigs.kD = kD;
+            PIDConfigs.kS = kS;
+            PIDConfigs.kV = kV;
+            PIDConfigs.kA = kA;
 
-            motor.getConfigurator().apply(PIDConfigs);
+            motor.getConfigurator().refresh(PIDConfigs);
         }
-
-        updateGains();
     }
 
 }
