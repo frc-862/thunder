cee940d797e01154904b38f233736a237e7b6d97
doorio <jusnoord@gmail.com>
2025-02-15 07:58:07 -0500

    i fixa da tupah!

--- util/Tuple.java
+++ util/Tuple.java
@@ -1,10 +1,23 @@
 package frc.thunder.util;
 
-public class Tuple<K, V> { 
+public class Tuple<K, V> extends Object { 
     public final K k;
     public final V v;
     public Tuple(K k, V v) { 
-      this.k = k; 
-      this.v = v; 
+        this.k = k; 
+        this.v = v; 
     } 
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Tuple<?, ?> other = (Tuple<?, ?>) obj;
+        return k.equals(other.k) && v.equals(other.v);
+    }
+
+    @Override
+    public int hashCode() {
+        return k.hashCode() ^ v.hashCode();
+    }
   } 
