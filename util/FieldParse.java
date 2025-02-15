190b9c89751fe92dd4e806e5c75147d519f8dcb4
jfiaoejf <tjwellswork@gmail.com>
2025-02-15 10:44:09 -0500

    [#188] - added function for parsing wpical output

---
+++ util/FieldParse.java
@@ -0,0 +1,29 @@
+package frc.thunder.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+
+public class FieldParse {
+    
+    public static AprilTagFieldLayout wpicalParse() throws IOException{ 
+        AprilTagFieldLayout reefscape = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+        
+        List<AprilTag> wpical = new AprilTagFieldLayout("~/nautilus/src/main/deploy/example.txt").getTags();
+        List<AprilTag> reefscapeTags = reefscape.getTags();
+
+
+
+        for(AprilTag tag : wpical){
+            reefscapeTags.set(tag.ID, tag);
+        }
+
+        return new AprilTagFieldLayout(reefscapeTags, reefscape.getFieldLength(), reefscape.getFieldWidth());
+    }
+}
