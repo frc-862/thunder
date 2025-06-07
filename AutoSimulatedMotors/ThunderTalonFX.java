8a92a28a398be36fb8510f4e38e28b31bd3514c9
vilok1 <vgunti777@gmail.com>
2025-06-07 10:23:47 -0400

    Create AutoSim Classes, simulate Talon & TalonFX

---
+++ AutoSimulatedMotors/ThunderTalonFX.java
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.thunder.AutoSimulatedMotors;
+
+import com.ctre.phoenix6.sim.ChassisReference;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.LinearSystemSim;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N2;
+import frc.robot.Robot;
+import frc.thunder.hardware.ThunderBird;
+
+/** Add your docs here. */
+public class ThunderTalonFX extends ThunderBird{
+
+    private TalonFXSimState simMotor;
+    private Notifier notifier;
+
+    public ThunderTalonFX(int deviceId, String canbus, boolean invert, double statorLimit, boolean brake) {
+        super(deviceId, canbus, invert, statorLimit, brake);
+
+        if (Robot.isSimulation()){
+            simMotor = super.getSimState();
+
+            simMotor.Orientation = invert ? ChassisReference.Clockwise_Positive
+                : ChassisReference.CounterClockwise_Positive;
+            
+            LinearSystemSim<N2, N1, N2> physicsSim = new LinearSystemSim<N2, N1, N2>(LinearSystemId.createDCMotorSystem(
+                0.55, 0.9));
+
+            notifier = new Notifier(() -> {
+
+                simMotor.setSupplyVoltage(RobotController.getBatteryVoltage());
+                
+                physicsSim.setInput(simMotor.getMotorVoltage());
+                physicsSim.update(0.02);
+
+
+                simMotor.setRotorVelocity(physicsSim.getOutput(0));
+            });
+
+            notifier.startPeriodic(0.02);
+        } 
+    }
+    
+
+    public void close() {
+        if (notifier != null) {
+            notifier.stop();
+        }
+
+        super.close();
+    }
+}
---
+++ AutoSimulatedMotors/ThunderTalonSRX.java
@@ -0,0 +1,73 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.thunder.AutoSimulatedMotors;
+
+
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import frc.robot.Robot;
+
+/** Add your docs here. */
+public class ThunderTalonSRX extends Talon{
+    
+    private double power;
+    private boolean isInverted = false;
+
+    /**
+     * Creates a new ThunderTalon.
+     *
+     * @param channel The PWM channel that the TalonSRX is connected to.
+     */
+    public ThunderTalonSRX(int channel) {
+        super(channel);
+    }
+
+    /**
+    * Set the PWM value.
+    *
+    * <p>The PWM value is set using a range of -1.0 to 1.0, appropriately scaling the value for the
+    * FPGA.
+    *
+    * <p>In simulation, this method will store the power value instead of sending to the hardware.
+    *
+    * @param speed The speed value between -1.0 and 1.0 to set.
+    */
+    @Override
+    public void set(double power) {
+
+        if (Robot.isSimulation()) this.power = isInverted ? -power : power;
+        else super.set(power);
+    }
+
+    /**
+    * Get the recently set value of the PWM. This value is affected by the inversion property. If you
+    * want the value that is sent directly to the MotorController, use {@link
+    * edu.wpi.first.wpilibj.PWM#getSpeed()} instead.
+
+    * <p>In simulation, this method will return the stored power value instead of reading from the hardware
+    *
+    * @return The most recently set value for the PWM between -1.0 and 1.0.
+    */
+    @Override
+    public double get() {
+        if (Robot.isSimulation()) return power;
+        else return super.get();
+    }
+
+    @Override
+    public void stopMotor(){
+        // Don't use set(0) as that will feed the watch kitty
+
+        if (Robot.isSimulation()) power = 0.0;
+        else super.stopMotor();
+    }
+
+    @Override
+    public void setInverted(boolean isInverted) {
+
+        if (Robot.isSimulation()) this.isInverted = isInverted;
+        else super.setInverted(isInverted);
+    }
+
+}
